5c8395044139b071b17a3994d43a1a6eb0a76955
githubandrea123 <miraandrea>
2022-02-25 08:27:48 -0500

    termine con la decoracion de el menu de administrador

--- src/Componete/IU/IconMenu/IconMenu.jsx
+++ src/Componete/IU/IconMenu/IconMenu.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { IoMdHome } from "react-icons/io";
 import { IoMdPersonAdd } from "react-icons/io";
+import { MdOutlineCloudDownload } from "react-icons/md";
 import { IoExitOutline } from "react-icons/io5";
+import { NavLink } from 'react-router-dom';
+import swal from 'sweetalert';
 import './IconMenu.css'
 
 export const IconMenu = () => {
   return (
     <div className='menu'>
-    
         <IoMdHome className='iconmenu'/>
         <p>Inicio</p>
-        <IoMdPersonAdd className='iconmenu' />
+        <IoMdPersonAdd onClick={()=>MstrarAlerta()} className='iconmenu' />
         <p>Registrar</p>
         {/* El icono de menu es opcional */}
         {/* <MdOutlineCloudDownload className='iconmenu' />
@@ -18,5 +20,10 @@ export const IconMenu = () => {
         <IoExitOutline className='iconmenu' />
         <p>Salir</p>
     </div>
+    
   )
+}
+const MstrarAlerta=()=>{
+  swal("Su usuario y contraseña es el documento de identidad")
+
 }
