4a4072fecd6ca1b85981aa18fe4b51fed20d306d
miraandrea <andreamira-10@outlook.com>
2022-02-25 13:11:26 -0500

    se instalaron las libreria npm install @sweetalert/with-react para la ventana modal

--- src/Componete/IU/IconMenu/IconMenu.jsx
+++ src/Componete/IU/IconMenu/IconMenu.jsx
@@ -4,7 +4,7 @@ import { IoMdPersonAdd } from "react-icons/io";
 import { MdOutlineCloudDownload } from "react-icons/md";
 import { IoExitOutline } from "react-icons/io5";
 import { NavLink } from 'react-router-dom';
-import swal from 'sweetalert';
+import swal from '@sweetalert/with-react';
 import './IconMenu.css'
 
 export const IconMenu = () => {
@@ -12,18 +12,24 @@ export const IconMenu = () => {
     <div className='menu'>
         <IoMdHome className='iconmenu'/>
         <p>Inicio</p>
-        <IoMdPersonAdd onClick={()=>MstrarAlerta()} className='iconmenu' />
-        <p>Registrar</p>
         {/* El icono de menu es opcional */}
         {/* <MdOutlineCloudDownload className='iconmenu' />
         <p>Importar</p> */}
         <IoExitOutline className='iconmenu' />
         <p>Salir</p>
+    <div>
+        <IoMdPersonAdd onClick={()=>MstrarAlerta()} className='iconmenu' /> 
+        <p>Registrar</p>
     </div>
-    
+    </div>
   )
 }
 const MstrarAlerta=()=>{
-  swal("Su usuario y contraseña es el documento de identidad")
-
+  swal (
+    <div>
+      <input type="text" placeholder='nombre'/>
+      <p>hola</p>
+      <button>kk</button>
+    </div>
+  )
 }
